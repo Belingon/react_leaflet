1a124a2b63fca3d492dd4024e8e656adb4bf30b8
Courtland <courtland.parker@sovos.com>
2017-06-20 16:43:36 -0400

    Updated the instructions for MultipleMarkerPage

--- src/components/ChoroplethMap.js
+++ src/components/ChoroplethMap.js
@@ -55,7 +55,9 @@ class ChoroplethMap extends Component {
                 <div className="titleContainer">
                     <label className="subTitle">Choropleth US States Map</label>
                 </div>
-                <p>This map uses census data to create a choropleth map based on population density</p>
+                <p>This map uses census data to create a choropleth map based on population density.<br/>
+                    Click anywhere to add more markers!
+                </p>
                 <Map id="choroplethMap" className="leaflet-choropleth-container" ref="choroplethMap"
                      center={this.state.latlng} zoom={this.state.zoom}>
                     <TileLayer
